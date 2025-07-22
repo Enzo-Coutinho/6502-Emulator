f94ac73455fe5c73407966365a39d0f25703a3f0
Enzo Coutinho <enzocoutinho71@gmail.com>
2025-07-22 20:29:01 -0300

    cpu and memory structs

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef __uint8_t Byte;
+typedef __uint16_t Word;
+typedef __uint32_t DWord;
+
+#define MAX_MEMORY (1024 * 64) 
+
+
+struct MEMORY
+{
+   Byte data[MAX_MEMORY]; 
+};
+
+struct CPU
+{
+    Word PC;
+    Word SP;
+
+    Byte _reg_accumulator, _reg_X, _reg_Y;
+
+    struct PROCESSOR_STATUS
+    {
+        Byte _carry : 1;
+        Byte _zero : 1;
+        Byte _interrupt_disable : 1;
+        Byte _decimal_mode : 1;
+        Byte : 1;
+        Byte _break_command : 1;
+        Byte _overflow : 1;
+        Byte _negative : 1;
+    } processor_status;
+};
+
+void reset_cpu(struct CPU * cpu, struct MEMORY * memory);
+void initialize_memory(struct MEMORY * memory);
+
+void reset_cpu(struct CPU * cpu, struct MEMORY * memory)
+{
+    cpu->PC = 0xFFFC;
+    cpu->SP = 0x0100;
+    cpu->_reg_accumulator = cpu->_reg_X = cpu->_reg_Y = 0;
+    cpu->processor_status._carry = 0;
+    cpu->processor_status._zero = 0;
+    cpu->processor_status._interrupt_disable = 0;
+    cpu->processor_status._decimal_mode = 0;
+    cpu->processor_status._break_command = 0;
+    cpu->processor_status._overflow = 0;
+    cpu->processor_status._negative = 0;
+
+    initialize_memory(memory);
+}
+
+void initialize_memory(struct MEMORY * memory)
+{
+    for(DWord i=0; i<MAX_MEMORY; i++)
+    {
+        memory->data[i] = 0;
+    }
+}
+
+int main()
+{
+    struct MEMORY memory;
+    struct CPU cpu;
+
+    reset_cpu(&cpu, &memory);
+
+    return 0;
+}
